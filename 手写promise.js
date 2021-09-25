3f8b1b62aa6f0cc16feff3b77feb1aee4bb70a14
yangyang825 <827847553@qq.com>
2021-09-25 11:44:56 +0800

    feat: 增加发布订阅模式,当状态改变时再把缓存中的函数拿出来执行

--- 手写promise.js
+++ 手写promise.js
@@ -7,15 +7,22 @@ class MyPromise {
     this.state = STATE_PENDING;
     this.resolveValue = undefined;
     this.rejectReason = undefined;
+    //发布订阅模式实现Promise内异步操作: then时如果state还是pendding,则注册回调函数
+    this.onResolvedCallback = [];
+    this.onRejectedCallback = [];
 
     let resolve = (value) => {
       this.state = STATE_RESOLVE;
       this.value = value;
+      //发布
+      this.onResolvedCallback.forEach(fn => fn());
     }
 
     let reject = (value) => {
       this.state = STATE_REJECT;
       this.reason = value;
+      //发布
+      this.onRejectedCallback.forEach(fn => fn());
     }
     executor(resolve, reject); //传进的executor参数同步执行
   };
@@ -26,12 +33,23 @@ class MyPromise {
     if (this.state === STATE_REJECT) {
       onRejected(this.reason);
     }
+    //如果执行then时还未resolve,则先缓存方法,监听resolve,resolve执行时顺便把缓存的函数拿出来执行
+    if (this.state === STATE_PENDING) {
+      this.onResolvedCallback.push(() => {
+        onFullFilled(this.value);
+      });
+      this.onRejectedCallback.push(() => {
+        onRejected(this.reason);
+      })
+    }
   }
 }
 
 new MyPromise((resolve, reject) => {
   // resolve(10);
-  reject(2000);
+  setTimeout(() => {
+    reject(2000);
+  }, 10)
   console.log(1);
 }).then(
   function(value) {
